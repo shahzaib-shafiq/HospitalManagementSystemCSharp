29fff79de5c6296009130a8b0f31c720a4eb5ba3
Muhammad Shahzaib Shafiq <f200317@cfd.nu.edu.pk>
2022-06-25 08:57:31 +0500

    Add project files.

---
+++ HospitalManagementSystemCSharp/PatientRegistration.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace HospitalManagementSystemCSharp
+{
+    public partial class PatientRegistration : Form
+    {
+        public PatientRegistration()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-A85V0ME\SQLEXPRESS;Initial Catalog=Hospitalmanagement;Integrated Security=True");
+            con.Open();
+            string gen = string.Empty;
+            if (radioButton1.Checked)
+            {
+                gen = "Male";
+            }
+            else
+            {
+                gen = "Female";
+            }
+            try
+            {
+                string str = "INSERT INTO patientinfo(id,name,gen,age,date,cont,addr,disease,status,r_type,building,r_no,price) VALUES('" + textBox1.Text +"','" + textBox2.Text + "','" + gen + "','" + textBox3.Text + "','" + textBox4.Text + "','" + textBox5.Text + "','" + textBox6.Text + "','" + textBox7.Text + "','" + textBox8.Text + "','" + textBox10.Text + "','" + textBox9.Text + "','" + textBox11.Text + "','" + textBox12.Text + "'); ";
+
+                SqlCommand cmd = new SqlCommand(str, con);
+                cmd.ExecuteNonQuery();
+                string str1 = "select max(Id) from patientinfo;";
+
+                SqlCommand cmd1 = new SqlCommand(str1, con);
+                SqlDataReader dr = cmd1.ExecuteReader();
+                if (dr.Read())
+                {
+                    MessageBox.Show("Patient Information Saved Successfully..");
+                    textBox2.Text = "";
+                    textBox3.Text = "";
+                    textBox4.Text = "";
+                    textBox5.Text = "";
+                    textBox6.Text = "";
+                    textBox7.Text = "";
+                    textBox8.Text = "";
+                    textBox9.Text = "";
+                    textBox10.Text = "";
+                    textBox11.Text = "";
+                    textBox12.Text = "";
+                                   
+                }                
+            }
+            catch (SqlException excep)
+            {
+                MessageBox.Show(excep.Message);
+            }
+            con.Close();
+        }
+
+        private void PatientRegistration_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-A85V0ME\SQLEXPRESS;Initial Catalog=Hospitalmanagement;Integrated Security=True");
+            con.Open();
+            string str1 = "select max(id) from patientinfo;";
+
+            SqlCommand cmd1 = new SqlCommand(str1, con);
+            SqlDataReader dr = cmd1.ExecuteReader();
+            if (dr.Read())
+            {
+                string val = dr[0].ToString();
+                if (val == "")
+                {
+                    textBox1.Text = "1";
+                }
+                else
+                {
+                    int a;
+                    a = Convert.ToInt32(dr[0].ToString());
+                    a = a + 1;
+                    textBox1.Text = a.ToString();
+                }
+            }
+            con.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+            textBox7.Text = "";
+            textBox8.Text = "";
+            textBox9.Text = "";
+            textBox10.Text = "";
+            textBox11.Text = "";
+            textBox12.Text = "";
+            textBox1.Text = "";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
